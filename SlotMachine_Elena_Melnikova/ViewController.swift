635973e658979a28f804ae2fc69f6434765e7538
canadianExperience <lena2157@mail.ru>
2019-01-11 12:52:55 -0500

    Initial Commit

---
+++ SlotMachine_Elena_Melnikova/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SlotMachine_Elena_Melnikova
+//
+//  Created by Elena Melnikova on 2019-01-11.
+//  Copyright © 2019 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblJackpot: UILabel!
+    @IBOutlet weak var lblCredits: UILabel!
+    @IBOutlet weak var lblBet: UILabel!
+    @IBOutlet weak var lblWinnings: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func quitGame(_ sender: UIButton) {
+    }
+    @IBAction func stepper(_ sender: UIStepper) {
+    }
+    
+    @IBAction func resetGame(_ sender: UIButton) {
+    }
+    @IBAction func spin(_ sender: UIButton) {
+    }
+}
+
